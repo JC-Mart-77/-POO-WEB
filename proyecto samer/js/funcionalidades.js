c64377e5e0ca434765ca91880e32988ef5c7d7f5
samer96 <morazan05@gmail.com>
2017-06-24 15:57:37 -0600

    mi parte
    mi parte

---
+++ proyecto samer/js/funcionalidades.js
@@ -0,0 +1,23 @@
+
+ function mouseEnter(){
+	document.getElementById("btn-seguir").style.backgroundColor = "#000";
+	document.getElementById("btn-seguir").style.color = "#fff";
+}
+
+function mouseLeave(){
+	document.getElementById("btn-seguir").style.backgroundColor = "#fff";
+	document.getElementById("btn-seguir").style.color = "#000";
+}
+
+
+function mouseEnter(){
+	document.getElementById("btn-puntos").style.backgroundColor = "#000";
+	document.getElementById("btn-puntos").style.color = "#fff";
+}
+
+function mouseLeave(){
+	document.getElementById("btn-puntos").style.backgroundColor = "#fff";
+	document.getElementById("btn-puntos").style.color = "#000";
+}
+
+
